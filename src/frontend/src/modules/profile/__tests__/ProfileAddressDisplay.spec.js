4b121f3cd38440e4908b232188da5100ab6a3374
Денис Васильев <corvus007@yandex.ru>
2021-11-02 23:19:48 +0300

    Создание тестов для компонента ProfileAddressDisplay модуля profile

---
+++ src/frontend/src/modules/profile/__tests__/ProfileAddressDisplay.spec.js
@@ -0,0 +1,45 @@
+import { createLocalVue, shallowMount } from "@vue/test-utils";
+import ProfileAddressDisplay from "@/modules/profile/components/ProfileAddressDisplay.vue";
+import { mocksOrder } from "@/store/mocks/mocks-order.js";
+import AppIcon from "@/common/components/AppIcon.vue";
+
+const localVue = createLocalVue();
+localVue.component("AppIcon", AppIcon);
+
+describe("ProfileAddressDisplay", () => {
+  let wrapper;
+  const propsData = {
+    counter: 1,
+    address: mocksOrder.orderAddress,
+  };
+  const createComponent = (options) => {
+    wrapper = shallowMount(ProfileAddressDisplay, options);
+  };
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  it("is rendered", () => {
+    createComponent({ localVue, propsData });
+    console.log(wrapper.html());
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  it("is output address name", () => {
+    createComponent({ localVue, propsData });
+    expect(wrapper.html()).toContain(propsData.address.name);
+  });
+
+  it("emit editing custom event when on click change address button", async () => {
+    createComponent({ localVue, propsData });
+    const changeAddrBtn = wrapper.find(`[data-test="change-address-button"]`);
+    /*
+     * TODO: Не понимаю, почему не срабатывает обычный клик по кнопке.
+     *       В учебном проекте, при взаимодействии с AppIcon в тестах все эмитится
+     */
+    // await changeAddrBtn.trigger("click");
+    await changeAddrBtn.vm.$emit("click");
+    expect(wrapper.emitted("editing")).toBeTruthy();
+  });
+});
