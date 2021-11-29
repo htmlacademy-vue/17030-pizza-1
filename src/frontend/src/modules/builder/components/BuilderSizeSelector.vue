2395e56c54d514ac200b5b9ce1d27effb1fec275
Денис Васильев <corvus007@yandex.ru>
2021-11-29 23:05:14 +0300

    Локальные стили для BuilderSizeRadio.vue

--- src/frontend/src/modules/builder/components/BuilderSizeSelector.vue
+++ src/frontend/src/modules/builder/components/BuilderSizeSelector.vue
@@ -3,30 +3,28 @@
     <h2 class="title title--small sheet__title">Выберите размер</h2>
 
     <div class="sheet__content diameter">
-      <AppSelectorRadio
+      <BuilderSizeRadio
         v-for="size in sizes"
         :key="size.name"
         v-model="pickedSize"
         :value="size.id"
-        class="diameter__input"
         :class="`diameter__input--${size.type}`"
-        name="size"
         data-test="size-radio-item"
       >
         <span>{{ size.name }}</span>
-      </AppSelectorRadio>
+      </BuilderSizeRadio>
     </div>
   </div>
 </template>
 
 <script>
-import AppSelectorRadio from "@/common/components/AppSelectorRadio";
 import { mapState } from "vuex";
+import BuilderSizeRadio from "@/modules/builder/components/BuilderSizeRadio.vue";
 
 export default {
   name: "BuilderSizeSelector",
   components: {
-    AppSelectorRadio,
+    BuilderSizeRadio,
   },
 
   computed: {
@@ -42,5 +40,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" scoped></style>
